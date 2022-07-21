dbfb8ccf88b10cd4d33abba26f3153cd31a14084
Mert Celikkol <mertcelikkol@gmail.com>
2022-07-21 14:51:36 +0300

    Initial Commit

---
+++ QuizVersion2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizVersion2
+//
+//  Created by Macbook on 21.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
